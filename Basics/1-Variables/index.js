826e80202c301f57c9e21bb7105c49ba01376877
Muhammad Shakir Khan <shakir.sedoc@gmail.com>
2022-02-10 16:43:39 +0500

    Variables Example

---
+++ Basics/1-Variables/index.js
@@ -0,0 +1,16 @@
+// Defination: 
+// We use variables for store data temporarily. 
+
+// Rules To Create Variables:
+// 1. Can't be a reserved keyword (Like: if, let, const).
+// 2. Should be meaning full and descriptive name.
+// 3. Can't start with numbers.
+// 4. They can't contain spaces or hypen.
+// 5. It is Case-sensitive (Like: firstName and FirstName both are different).
+// 6. Should be in Camel Notations, (It is a best practice like: firstName).
+
+// Important:
+// By Default the value is undefined in JS. (like: let name; It will return undefined).
+
+let myName = 'Shakir';
+console.log(myName);
