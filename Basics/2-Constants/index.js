12864ee59942926c0276f55ab94f9b3cf88494ac
Muhammad Shakir Khan <shakir.sedoc@gmail.com>
2022-02-10 16:56:54 +0500

    New: Constants Example

---
+++ Basics/2-Constants/index.js
@@ -0,0 +1,7 @@
+// Important Point:
+// We can't reassign values in constant.
+
+// Example:
+
+const myName = "Muhammad Shakir Khan";
+console.log(myName);
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # JavaScript Fundamentals By Muhammad Shakir Khan
 ## Basics
 * 1. Variables
+* 2. Constants
