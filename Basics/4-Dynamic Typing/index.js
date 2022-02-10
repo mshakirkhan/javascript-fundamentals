f089d7ef4fe83a5388aa98cf3abc2918d0752769
Muhammad Shakir Khan <shakir.sedoc@gmail.com>
2022-02-10 17:52:41 +0500

    New: Dynamic Typing Example

---
+++ Basics/4-Dynamic Typing/index.js
@@ -0,0 +1,9 @@
+// Important Points (Dynamic Typing)
+// JavaScript is a dynamic language, which means it is dynamically-typed.
+
+// Examples:
+
+let myValue = "Muhammad Shakir Khan";
+myValue = 10;
+
+// Note: As we can see we can change the value type in run time
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 * 1. Variables (let)
 * 2. Constants (const)
 * 3. Primitive Types (String, Number, Boolean, Undefined, Null)
+* 4. Dynamic Typing
