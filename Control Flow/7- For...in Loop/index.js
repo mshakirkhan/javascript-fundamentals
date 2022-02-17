a777228df1bd624fec3d72128e8fb3fd5b30f18f
Muhammad Shakir Khan <shakir.sedoc@gmail.com>
2022-02-17 17:16:23 +0500

    Update: For-in loop Example

--- Control Flow/7- For...in Loop/index.js
+++ Control Flow/7- For...in Loop/index.js
@@ -1,4 +1,6 @@
-// For-in loop Example:
+// For-in loop 
+
+// Example 01:
 const person = {
     name : "Muhammad Shakir Khan",
     age : "27"
@@ -12,3 +14,16 @@ for(let key in person) {
 
 // name Muhammad Shakir Khan
 // age 27
+
+// Example 02:
+
+const phones = ['IPhone', 'Sumsung','Realme'];
+for(let index in phones) {
+    console.log(index, phones[index]);
+}
+
+// Result:
+
+// 0 IPhone
+// 1 Sumsung
+// 2 Realme
