098e539d1e1d4dfbfd4139913d43ac7e2782eb83
Muhammad Shakir Khan <shakir.sedoc@gmail.com>
2022-02-17 11:37:25 +0500

    New: Operators Precedence Example

---
+++ Operators/10-Operators Precedence/index.js
@@ -0,0 +1,9 @@
+// Example 01
+let result = 2 + 3 * 5;
+console.log(result); 
+// The result will be 17, the reason is Multiplication has higher Precedence that's why it will multiple first than add.
+
+// Example 02
+let resultwo = (5 + 5) * 2;
+console.log(resultwo);
+// The result will be 20, here we have changed the Precedence with the help of (), so it will be add first than multiply.
