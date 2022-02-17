962b84943aa0680cec7f2db4c00a344139a9fbd9
Muhammad Shakir Khan <shakir.sedoc@gmail.com>
2022-02-17 12:34:09 +0500

    New: Switch...case Example

---
+++ Control Flow/2-Switch...case/index.js
@@ -0,0 +1,15 @@
+// Same as IF Else Conditions
+// Example
+
+let role = 'guest';
+
+switch (role) {
+    case 'guest':
+        console.log("Guest User");
+        break;
+    case 'moderator':
+        console.log('Moderator User');
+        break;
+    default:
+        console.log('Unlnown User');
+}
