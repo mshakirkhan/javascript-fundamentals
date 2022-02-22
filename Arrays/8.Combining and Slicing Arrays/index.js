134fda271aaa72b3a69ca1797eb991ca140406fe
Muhammad Shakir Khan <shakir.sedoc@gmail.com>
2022-02-22 10:29:22 +0500

    New: Combining and Slicing Arrays Example

---
+++ Arrays/8.Combining and Slicing Arrays/index.js
@@ -0,0 +1,11 @@
+// Combining Array Example
+let arrayOne = [1,2,3,4,5];
+let arrayTwo = [6,7,8,9,10];
+
+let combinedArray = arrayOne.concat(arrayTwo);
+console.log(combinedArray);
+
+// Slicing Array Example
+// Start and End if we passed only one parameter that will be starting point
+let sliceArray = combinedArray.slice(2,4); 
+console.log(sliceArray);
